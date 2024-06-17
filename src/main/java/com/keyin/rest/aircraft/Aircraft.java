9cf9347ba522431966d8be7f1c9d2e2e21b19247
malerie-earle <malerie.earle@keyin.com>
2024-06-17 21:09:13 -0230

    classes finished, json data added, data reader added, flight generator idea started

--- src/main/java/com/keyin/rest/aircraft/Aircraft.java
+++ src/main/java/com/keyin/rest/aircraft/Aircraft.java
@@ -1,4 +1,57 @@
 package com.keyin.rest.aircraft;
 
 public class Aircraft {
+    long aircraft_ID;
+    String model;
+    String manufacturer;
+    String airline;
+    int capacity;
+
+    public Aircraft(long aircraft_ID, String model, String manufacturer, String airline, int capacity) {
+        this.aircraft_ID = aircraft_ID;
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.airline = airline;
+        this.capacity = capacity;
+    }
+
+    public long getAircraft_ID() {
+        return aircraft_ID;
+    }
+
+    public void setAircraft_ID(long aircraft_ID) {
+        this.aircraft_ID = aircraft_ID;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getAirline() {
+        return airline;
+    }
+
+    public void setAirline(String airline) {
+        this.airline = airline;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
 }
