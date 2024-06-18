05f62d41b305db38066283405f5ee8415240532b
NLScotty <Scott.B.Normore@gmail.com>
2024-06-18 14:08:54 -0230

    Updated Aircraft Models
    Brainstorming on the other Controller Tests atm; might switch gears to either other tests or Aircraft Service

--- src/test/java/com/keyin/models/AircraftModelTests.java
+++ src/test/java/com/keyin/models/AircraftModelTests.java
@@ -7,13 +7,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 public class AircraftModelTests {
     // need to change based on contsructor
     public void testEquals(){
-        Aircraft ac1 = new Aircraft(1,"Airbus A220-300","Air Canada",132);
-        Aircraft ac2 = new Aircraft(1,"Airbus A220-300","Air Canada",132);
+        Aircraft ac1 = new Aircraft(1,"A220-300","Airbus Corp","Scott's Airline",132);
+        Aircraft ac2 = new Aircraft(1,"A220-300","Airbus Corp","Scott's Airline",132);
         Assertions.assertEquals(ac1, ac2);
     }
     public void testNotEquals(){
-        Aircraft ac1 = new Aircraft(1,"Airbus A220-300","Air Canada",132);
-        Aircraft ac2 = new Aircraft(2,"Airtrain T9000","Off the Rails",200);
+        Aircraft ac1 = new Aircraft(1,"A220-300","Airbus Corp","Scott's Airline",132);
+        Aircraft ac2 = new Aircraft(1,"A220-300","Airbus Corp","Not Scott's Airline",132);
         Assertions.assertEquals(ac1,ac2);
     }
 }
