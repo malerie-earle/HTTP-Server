33d555fae61804f50c1e251c17917567755a1dbf
anzhelikakuchma <anzhelika.kuchma@gmail.com>
2024-06-19 13:19:40 -0230

    ADD A NEW AIRPORT and LIST ALL AIRPORTS  mathods complited

--- src/main/java/com/keyin/rest/airport/AirportController.java
+++ src/main/java/com/keyin/rest/airport/AirportController.java
@@ -1,4 +1,27 @@
 package com.keyin.rest.airport;
 
+import com.keyin.rest.airport.Airport;
+import com.keyin.rest.airport.AirportService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
 public class AirportController {
+    @Autowired
+    private AirportService airportService;
+
+
+    @PostMapping("airport")
+    public Airport createAirport (@RequestBody Airport newAirport){
+        return airportService.createAirport(newAirport);
+    }
+
+    @GetMapping("airports")
+    public List<Airport> getAllAirports(){
+        return airportService.getAllAirports();
+    }
+
 }
--- src/main/java/com/keyin/rest/airport/AirportService.java
+++ src/main/java/com/keyin/rest/airport/AirportService.java
@@ -1,4 +1,26 @@
 package com.keyin.rest.airport;
 
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
 public class AirportService {
+    private static Map<Integer, Airport> airportMap = new HashMap<Integer, Airport>();
+
+    // ADDING A NEW AIRPORT
+    public Airport createAirport(Airport newAirport) {
+        airportMap.put(airportMap.size() + 1, newAirport);
+
+        return newAirport;
+    }
+
+    // LOADING ALL AIRPORTS
+    public List<Airport> getAllAirports() {
+        return List.copyOf(airportMap.values());
+    }
+
 }
