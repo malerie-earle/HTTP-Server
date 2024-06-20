d4ff9de3f2b359c111d75e8f55ad8780c76ec560
NLScotty <Scott.B.Normore@gmail.com>
2024-06-20 14:34:11 -0230

    Flight Controller partially completed; waiting on class

--- src/main/java/com/keyin/rest/flight/FlightController.java
+++ src/main/java/com/keyin/rest/flight/FlightController.java
@@ -1,4 +1,39 @@
 package com.keyin.rest.flight;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
 public class FlightController {
+    @Autowired
+    private FlightService flightService = new FlightService();
+
+    @PostMapping("flight")
+    public Flight createFlight (@RequestBody Flight newFlight){
+        return flightService.createNewFlight(newFlight);
+    }
+
+    @GetMapping("flights")
+    public List<Flight> getAllFlights(){
+        return flightService.getAllFlight();
+    }
+
+    @GetMapping("flight/{id}")
+    public Flight getFlight(@PathVariable Long id){
+        return flightService.getFlightByID(id);
+    }
+
+    @PutMapping("flight/{id}")
+    public Flight updateAircaft(@PathVariable Long id, @RequestBody Flight updatedFlight){
+        return flightService.updateFlightByID(id, updatedFlight);
+    }
+
+    @DeleteMapping("flight/{id}")
+    public Flight deleteFlight(@PathVariable Long id){
+        return flightService.deleteFlight(id);
+    }
+
 }
