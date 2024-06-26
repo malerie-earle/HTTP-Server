ba2a7daaaec905362ad6d7f050c6e8e935ee0b39
NLScotty <Scott.B.Normore@gmail.com>
2024-06-25 21:39:37 -0230

    Some minor edits to Booking Service. CRUD operations for bookings now in Flight Controller
    Also added search flights by airport. Gonna need to test these

--- src/main/java/com/keyin/rest/flight/FlightController.java
+++ src/main/java/com/keyin/rest/flight/FlightController.java
@@ -57,6 +57,12 @@ public class FlightController {
         return allFlights.stream().filter(ap -> ap.getOrigin().getAirport_ID() == id).toList();
     }
 
+    @GetMapping("flights/airport/{id}")
+    public List<Flight> getFlightsByAirportID(@PathVariable Long id){
+        List<Flight> allFlights = flightService.getAllFlight();
+        return allFlights.stream().filter(ap -> (ap.getOrigin().getAirport_ID() == id || ap.getDestination().getAirport_ID() == id )).toList();
+    }
+
     @GetMapping("flights/airline/{airline}")
     public List<Flight> getFlightByAirLine(@PathVariable String airline){
         List<Flight> allFlights = flightService.getAllFlight();
@@ -76,27 +82,66 @@ public class FlightController {
         }
     }
 
+    @PutMapping("flight/book/update/{id}")
+    public Booking updateBooking(@PathVariable Long id, @RequestBody Booking booking){
+        Flight targetFlight = flightService.getFlightByID(id);
+        if(targetFlight != null && targetFlight.isOccupied(booking.getSeatRow(), booking.getSeatColumn())){
+            Booking oldBooking = bookingService.getBookingByID(id);
+            Booking updatedBooking = bookingService.updateBookingByID(id, booking);
+            targetFlight.setSeat(booking.getSeatRow(), booking.getSeatColumn(), updatedBooking);
+            targetFlight.setSeat(oldBooking.getSeatRow(), oldBooking.getSeatColumn(), null);
+            return updatedBooking;
+        }else{
+            return null;
+        }
+    }
+
+    @DeleteMapping ("flight/book/delete/{id}")
+    public Booking deleteBooking(@PathVariable Long id){
+        Flight targetFlight = flightService.getFlightByID(id);
+        if(targetFlight != null){
+            Booking deletedBooking = bookingService.deleteBooking(id);
+            targetFlight.setSeat(deletedBooking.getSeatRow(), deletedBooking.getSeatColumn(), null);
+            return deletedBooking;
+        }else{
+            return null;
+        }
+    }
+
+    @PutMapping("flight/book/delete")
+    public Booking updateBooking(@RequestBody Booking booking){
+        Flight targetFlight = flightService.getFlightByID(booking.getFlight_ID());
+        if(targetFlight != null){
+            Booking updatedBooking = bookingService.updateBookingByID(booking.getBooking_ID(), booking);
+            targetFlight.setSeat(booking.getSeatRow(), booking.getSeatColumn(), updatedBooking);
+            return updatedBooking;
+        }else{
+            return null;
+        }
+    }
+
     @GetMapping("bookings")
     public List<Booking> getAllBookings(){
         return bookingService.getAllBooking();
     }
 
     @GetMapping("booking/{id}")
-    public Booking getBookingbyBookingID(@PathVariable Integer id){
+    public Booking getBookingbyBookingID(@PathVariable Long id){
         return bookingService.getBookingByID(id);
     }
 
     @GetMapping("bookings/passenger/{id}")
-    public List<Booking> getBookingsByPassengerID(@PathVariable Integer id){
+    public List<Booking> getBookingsByPassengerID(@PathVariable Long id){
         List<Booking> allBookings = bookingService.getAllBooking();
         return allBookings.stream().filter(b -> b.getPassenger_ID() == id).toList();
     }
 
     //unsure if needed
     @GetMapping("bookings/flight/{id}")
-    public List<Booking> getBookingsByFlightID(@PathVariable Integer id){
+    public List<Booking> getBookingsByFlightID(@PathVariable Long id){
         List<Booking> allBookings = bookingService.getAllBooking();
         return allBookings.stream().filter(b -> b.getFlight_ID() == id).toList();
     }
+
 
 }
